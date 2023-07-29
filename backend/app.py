a43a97ad1a87be3be97f5196fa224fbac529709a
ywcheng <chengyinwu@qiniu.com>
2023-07-29 21:35:50 +0800

    fix app.py

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,32 @@
-from flask import Flask
+from flask import Flask, request, g
 from flask_sqlalchemy import SQLAlchemy
 import os
-import subprocess
-from flask import request
 
 app = Flask(__name__)
 
-# This keeps the warning messages away
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_uri = os.environ.get('DATABASE_URI')
 db_username = os.environ.get('DATABASE_USERNAME')
 db_password = os.environ.get('DATABASE_PASSWORD')
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://"+ db_username +":"+ db_password +"@" + \
     db_uri+"/postgres"
-
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 db.init_app(app)
 
+engine = None
+
+@app.before_request
+def before_request():
+    global engine
+    if not engine:
+        engine = db.engine
+    g.db = engine.connect()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 class Text(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,22 +38,13 @@ class Text(db.Model):
     def __repr__(self):
         return f"<Text {self.text}>"
 
-
 @app.route('/healthy')
 def healthy():
     return {"healthy": True}, 200
 
-
 @app.route('/host_name')
 def host_name():
     return {"host_name": os.environ.get('HOSTNAME')}, 200
-
-
-@app.route('/ab')
-def ab():
-    subprocess.run(["ab", "-c", "50", "-n", "10000", "http://127.0.0.1:5000/"])
-    return {"ab": True}, 200
-
 
 @app.route('/fetch')
 def fetch():
@@ -54,7 +55,6 @@ def fetch():
         } for word in words]
     return {"texts": results}, 200
 
-
 @app.route('/add', methods=['POST'])
 def add():
     text = request.json['text']
@@ -62,13 +62,11 @@ def add():
     db.session.commit()
     return 'Done', 201
 
-
 @app.route('/delete', methods=['DELETE'])
 def delete():
     db.session.query(Text).delete()
     db.session.commit()
     return 'Done', 200
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
